b15e096a16726ad532db26fc4c1de2277cf36466
raihanpf22 <raihanpambagyo@.com>
2022-11-03 21:54:54 +0700

    Update ya ges ya

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -5,12 +5,12 @@ class App {
   }
 
   async init() {
-    await this.load();
     this.btnSearchCars.onclick = this.run;
     
   }
 
-  run = () => {
+  run = async() => {
+    await this.load();
     Car.list.forEach((car) => {
       const node = document.createElement("div");
       node.innerHTML = car.render();
@@ -21,16 +21,16 @@ class App {
   // 
 
   async load() {
+
     const driver = document.getElementById("driver");
     const passenger = document.getElementById("passanger");
 
-    console.log(driver)
 
     const filterer = {driver: driver.value, passenger: passenger.value};
 
-    console.log(filterer);
 
     const cars = await Binar.listCars(filterer);
+    console.log(cars);
     Car.init(cars);
   }
 
